afc6066fd9f872f15fd331dd1fed8880ddb716e4
Toby Bi <toby.bi@outlook.com>
2021-02-19 21:40:44 +0100

    documentation and clean

--- helpers.py
+++ helpers.py
@@ -1,66 +1,77 @@
-def replace_any_bit(val, pos, new_bit):
-    """
-    Replace bit at position (starting at 0) with new bit in given value
+def replace_any_bit(val: int, pos: int, new_bit: int) -> int:
+    """Replace bit at position (starting at 0) with new bit.
 
     Parameters
     ----------
     val : int
-        Int to have bit replaced
+        Integer to have bit replaced.
     pos : int
-        Position to replace starting at 0 from LSB (right)
+        Position to replace starting at 0 from LSB (right).
     new_bit : int
-        0 or 1
+        0 or 1.
 
     Returns
     -------
-    ans : int
-        result
-    """
-    part1 = (val & (~1 << pos))     # replaces bit at pos with 0
-    part2 = (new_bit << pos)        # shifts new_bit to pos
-    ans = part1 | part2             # replaces 0 with new_bit at pos
-    return ans
+    replaced : int
+        Integer with changed bit.
 
-def binary_coarsen(val, coarsen):
+    Examples
+    --------
+    >>> replace_any_bit(10, 2, 0)
+    8
     """
-    Coarsen binary value by any integer amount.
+    part1 = val & (~1 << pos)       # replaces bit at pos with 0
+    part2 = new_bit << pos          # shifts new_bit to pos
+    replaced = part1 | part2             # replaces 0 with new_bit at pos
+    return replaced
+
+def binary_coarsen(val: int, coarsen: int) -> int:
+    """Coarsen binary value by any integer amount and set to middle bit.
 
     Parameters
     ----------
     val : int
-        Int to coarsen, unsigned
+        Integer to coarsen, unsigned.
     coarsen : int
-        bit value to coarsen by
+        Bit value to coarsen by.
 
     Returns
     -------
     val : int
-        final value
+        Coarsened value.
+
+    Examples
+    --------
+    >>> binary_coarsen(192830999, 4)
+    192831000
     """
     if coarsen == 4:
-        val = binary_coarsen_4bits(val)
+        # special case to coarsen by 4 for speediness
+        coarsened = ((val >> 4) << 4) | 8     # 8 is "1000" in binary
     else:
         for k in range(coarsen):
             if k < (coarsen - 1):
-                val = replace_any_bit(val, k, 0)    # replace every LSB from coarsen amount by 0
+                # replace every LSB from coarsen amount by 0
+                coarsened = replace_any_bit(val, k, 0)    
             else:
-                val = replace_any_bit(val, k, 1)    # replace coarsen amount pos by 1
+                # replace coarsen amount pos by 1
+                coarsened = replace_any_bit(val, k, 1)    
 
-    return val
+    return coarsened
 
-def binary_coarsen_4bits(val):
-    """
-    Coarsen binary value by 4 bits and set to the middle bit
+# def binary_coarsen_4bits(val):
+#     """
+#     Coarsen binary value by 4 bits and set to the middle bit
 
-    Parameters
-    ----------
-    val : int
-        Int to coarsen
+#     Parameters
+#     ----------
+#     val : int
+#         Int to coarsen
 
-    Returns
-    -------
-    ans : int
-        val coarsened by 4 bits 
-    """
-    ans = ((val >> 4) << 4) | 8     # 8 is "1000" in binary
-    return ans
+#     Returns
+#     -------
+#     ans : int
+#         val coarsened by 4 bits 
+#     """
+#     ans = ((val >> 4) << 4) | 8     # 8 is "1000" in binary
+#     return ans
